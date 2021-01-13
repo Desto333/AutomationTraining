7ebc2ccf2d9f8ff8b3f09e875b5f802053bb2b10
Andrei Lazarchik <desto@mail.ru>
2021-01-13 23:21:38 +0300

    add Hardcore task from WebDriver section

---
+++ webdriver/src/test/java/hardcore/page/CloudGoogleSearchResultPage.java
@@ -0,0 +1,20 @@
+package hardcore.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CloudGoogleSearchResultPage extends AbstractPage {
+
+    public CloudGoogleSearchResultPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public CloudGooglePricingCalculatorPage followTheLinkNamed(String linkName) {
+        new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+                .until(ExpectedConditions.elementToBeClickable(By.linkText(linkName)));
+        driver.findElement(By.linkText(linkName)).click();
+        return new CloudGooglePricingCalculatorPage(driver);
+    }
+}
