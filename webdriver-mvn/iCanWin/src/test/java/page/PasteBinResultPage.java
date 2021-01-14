858ce23e53f2f1df695c91827e1165cca96eabbd
Andrei Lazarchik <desto@mail.ru>
2021-01-14 16:38:41 +0300

    add I can win task from Webdriver section

---
+++ webdriver-mvn/iCanWin/src/test/java/page/PasteBinResultPage.java
@@ -0,0 +1,10 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+
+public class PasteBinResultPage extends AbstractPage{
+
+    public PasteBinResultPage(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ webdriver-mvn/iCanWin/src/test/java/test/CreateNewPasteAtPasteBinScript.java
@@ -0,0 +1,34 @@
+package test;
+
+import page.PasteBinHomePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class CreateNewPasteAtPasteBinScript {
+    private WebDriver driver;
+
+    @BeforeMethod(alwaysRun = true)
+    public void browserSetup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Programs\\Selenium\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void createNewPasteAtPasteBin() {
+        new PasteBinHomePage(driver)
+                .openPage()
+                .pasteCode("Hello from WebDriver")
+                .setPasteExpirationTime("10 Minutes")
+                .pasteNameOrTitle("helloweb")
+                .submitInput();
+    }
+
+    @AfterMethod
+    public void browserShutDown() {
+        driver.quit();
+        driver = null;
+    }
+}
