cc99e581979124933c791a3a77a7f8acbcd61dc7
Andrei Lazarchik <desto@mail.ru>
2021-01-22 00:03:23 +0300

    fixed Hurt me plenty task from Webdriver section

--- webdriver-mvn/hurtMePlenty/src/test/java/test/PriceForGooglePlatformRentTest.java
+++ webdriver-mvn/hurtMePlenty/src/test/java/test/PriceForGooglePlatformRentTest.java
@@ -1,13 +1,13 @@
 package test;
 
-import org.testng.Assert;
-import page.CalculationResultPage;
-import page.CloudGoogleStartPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import page.CalculationResultPage;
+import page.CloudGoogleStartPage;
 
 public class PriceForGooglePlatformRentTest {
     private WebDriver driver;
