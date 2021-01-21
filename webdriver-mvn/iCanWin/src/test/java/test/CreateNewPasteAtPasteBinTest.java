79010a915ab7508425dbdd09c29702856bb49c12
Andrei Lazarchik <desto@mail.ru>
2021-01-21 15:26:46 +0300

    fixed I can win task from Webdriver section

--- webdriver-mvn/iCanWin/src/test/java/test/CreateNewPasteAtPasteBinTest.java
+++ webdriver-mvn/iCanWin/src/test/java/test/CreateNewPasteAtPasteBinTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class CreateNewPasteAtPasteBinScript {
+public class CreateNewPasteAtPasteBinTest {
     private WebDriver driver;
 
     @BeforeMethod(alwaysRun = true)
